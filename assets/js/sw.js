a7278ff474de6cea778c252f5ccab00d17b6ff5d
mistermochi <36605549+mistermochi@users.noreply.github.com>
2018-06-30 23:11:20 +0800

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -17,13 +17,4 @@ const filesToCache = [
   {% for file in site.static_files %}
     '{{ file.path }}',
   {% endfor %}
-
-  // can be automated rather than manual entries
-  "/assets/images/bhavri-github-callbacks.png",
-  "/assets/images/bhavri-github-issues.png",
-  "/assets/images/jakethecake-svg-line-anime.png",
-  "/assets/images/svg-animated-mast-text-shapes-tweet.png",
-  "css/main.css",
-  "/about/",
-  "/index.html"
 ];
