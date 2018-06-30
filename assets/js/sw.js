9c8b6f54f8719d299934fba3ddfd902b6b9755a0
mistermochi <36605549+mistermochi@users.noreply.github.com>
2018-06-30 23:04:44 +0800

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,1 +1,27 @@
+//sw.js
+---
+---
 
+const version = '{{site.time | date: '%Y%m%d%H%M%S'}}';
+const staticCacheName = "openmedicshk::"+version;
+
+console.log("installing worker");
+
+const filesToCache = [
+  "/",
+  {% for page in site.html_pages %}
+    '{{ page.url }}',
+  {% endfor %}
+  {% for post in site.posts %}
+    '{{ post.url }}',
+  {% endfor %}
+
+  // can be automated rather than manual entries
+  "/assets/images/bhavri-github-callbacks.png",
+  "/assets/images/bhavri-github-issues.png",
+  "/assets/images/jakethecake-svg-line-anime.png",
+  "/assets/images/svg-animated-mast-text-shapes-tweet.png",
+  "css/main.css",
+  "/about/",
+  "/index.html"
+];
